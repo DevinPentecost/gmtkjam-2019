df1169cab1038ad8a0b2622926c7c0ebf155340f
Devin Pentecost <devinpentecost@gmail.com>
2019-08-03 14:32:43 -0700

    RoadGraph can make itself based on the node

--- project/objects/roadgraph/RoadGraph.gd
+++ project/objects/roadgraph/RoadGraph.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
+const RoadGraphNode = preload("res://objects/roadgraph/RoadGraphNode.tscn")
+const RoadGraphConnection = preload("res://objects/roadgraph/RoadGraphConnection.tscn")
+
 var connection_map = {}
 
 # Debug stuff
@@ -13,20 +16,87 @@ func _ready():
 func _process(delta):
 	# Every step we will check what the cars are trying to do
 	# We will turn the node they selected a different color
-	var new_obstacle_path = get_node("../Car").target_path
-	var new_player_path = get_node("../PlayerCar").target_path
+	return
+	"""
+	for car in get_tree().get_nodes_in_group('car'):
+		var car_path = car.target_path
+		if new_obstacle_path != obstacle_path:
+			if obstacle_path != null:
+				obstacle_path[0].modulate = Color(1, 1, 1, 1)
+			obstacle_path = new_obstacle_path
+			obstacle_path[0].modulate = Color(1, 0, 0, 1)
+		if new_player_path != player_path:
+			if player_path != null:
+				player_path[0].modulate = Color(1, 1, 1, 1)
+			player_path = new_player_path
+			player_path[0].modulate = Color(0, 1, 0, 1)
+	"""
+
+func build_from_game_map(game_map):
 	
-	if new_obstacle_path != obstacle_path:
-		if obstacle_path != null:
-			obstacle_path[0].modulate = Color(1, 1, 1, 1)
-		obstacle_path = new_obstacle_path
-		obstacle_path[0].modulate = Color(1, 0, 0, 1)
-	if new_player_path != player_path:
-		if player_path != null:
-			player_path[0].modulate = Color(1, 1, 1, 1)
-		player_path = new_player_path
-		player_path[0].modulate = Color(0, 1, 0, 1)
+	#We have a set of tile indices that represent intersections
+	var intersection_tiles = [1, 2, 3, 4, 5, 13, 14, 15, 16]
+	var nw_road = 10
+	var ne_road = 9
+	var sw_road = 12
+	var se_road = 11
 	
+	#Let's build points first for every intersection
+	var intersection_cells = {} #Position (Vec2 x y)
+	for intersection_tile in intersection_tiles:
+		
+		#Get the indices for all of these
+		for cell_position in game_map.get_used_cells_by_id(intersection_tile):
+			
+			#Make a new point at this place
+			var new_node = RoadGraphNode.instance()
+			
+			#Isometric so this gets tricky
+			var final_position = game_map.map_to_world(cell_position) / 4
+			final_position.y += 64
+			new_node.position = final_position
+			$RoadGraphNodes.add_child(new_node)
+			
+			#Store it for later
+			intersection_cells[cell_position] = new_node
+	
+	#Now go through each intersection
+	for intersection_cell in intersection_cells:
+		
+		#Check each direction
+		for direction in [[-1, 0], [0, 1], [0, -1], [1, 0]]:
+			
+			#Take a single step that direction
+			var directionv = Vector2(direction[0], direction[1])
+			var step_position = intersection_cell + directionv
+			var step_tile = game_map.get_cellv(step_position)
+			var valid_direction = false
+			match direction:
+				[-1, 0]:
+					#NW
+					valid_direction = step_tile == nw_road
+				[0, -1]:
+					#NE
+					valid_direction = step_tile == ne_road
+				[0, 1]:
+					#SW
+					valid_direction = step_tile == sw_road
+				[1, 0]:
+					#SE
+					valid_direction = step_tile == se_road
+			
+			#Was this a good direction to go?
+			if valid_direction:
+				var connecting_intersection_position = intersection_cell + (directionv * 5)
+				var target_cell = intersection_cells.get(connecting_intersection_position)
+				if target_cell:
+					
+					#We can make a connection
+					var new_connection = RoadGraphConnection.instance()
+					new_connection._node_a = intersection_cells[intersection_cell]
+					new_connection._node_b = target_cell
+					$RoadGraphConnections.add_child(new_connection)
+					
 func _build_connections():
 	
 	#Go through each connection
